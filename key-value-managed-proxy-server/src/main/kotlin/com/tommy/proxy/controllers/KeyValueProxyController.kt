e7f8a29f18e50fbc4f66488f95e27a18fc03d9d9
limhangyeol <hang19663@gmail.com>
2023-03-20 20:27:03 +0900

    인입된 요청 Key-Value 를 인접 노드에서 가져오는 API 구현
    work items: #11

--- key-value-managed-proxy-server/src/main/kotlin/com/tommy/proxy/controllers/KeyValueProxyController.kt
+++ key-value-managed-proxy-server/src/main/kotlin/com/tommy/proxy/controllers/KeyValueProxyController.kt
@@ -1,12 +1,15 @@
 package com.tommy.proxy.controllers
 
+import com.tommy.proxy.dtos.KeyValueGetResponse
 import com.tommy.proxy.dtos.KeyValueSaveRequest
 import com.tommy.proxy.dtos.KeyValueSaveResponse
 import com.tommy.proxy.services.KeyValueProxyService
 import com.tommy.proxy.utils.IpUtil
 import jakarta.servlet.http.HttpServletRequest
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -21,5 +24,14 @@ class KeyValueProxyController(
     ): KeyValueSaveResponse {
         val clientIp = IpUtil.getClientIp(request)
         return keyValueProxyService.put(clientIp, keyValueSaveRequest)
+    }
+
+    @GetMapping
+    fun get(
+        @RequestParam key: String,
+        request: HttpServletRequest,
+    ): KeyValueGetResponse {
+        val clientIp = IpUtil.getClientIp(request)
+        return keyValueProxyService.get(clientIp, key)
     }
 }
