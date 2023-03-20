09f11ebed3c5ad080c3654c70d9956d2c9e09042
limhangyeol <hang19663@gmail.com>
2023-03-20 16:11:06 +0900

    key-value-store-application dependency 추가
    * key-value-store 의존성 추가
    * spring mockk 의존성 추가

--- key-value-store-application/build.gradle.kts
+++ key-value-store-application/build.gradle.kts
@@ -11,6 +11,8 @@ tasks.getByPath("jar").enabled = false
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation(project(":key-value-store"))
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
 }
