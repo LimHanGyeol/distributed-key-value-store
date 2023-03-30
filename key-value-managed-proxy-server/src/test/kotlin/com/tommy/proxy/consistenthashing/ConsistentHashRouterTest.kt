4eaf610d038864996888bb20454da33e304ae9e8
limhangyeol <hang19663@gmail.com>
2023-03-30 15:55:54 +0900

    테스트 실패 개선
    * 노드 정보 Property 로 주입 받을 수 있게 변경의 전파사항
    * 해당 테스트를 개선한다.
    
    work items: #17

--- key-value-managed-proxy-server/src/test/kotlin/com/tommy/proxy/consistenthashing/ConsistentHashRouterTest.kt
+++ key-value-managed-proxy-server/src/test/kotlin/com/tommy/proxy/consistenthashing/ConsistentHashRouterTest.kt
@@ -12,10 +12,10 @@ class ConsistentHashRouterTest {
     fun `init consistent hash router`() {
         // Arrange
         val virtualNodeCount = 10
-        val node1 = Instance("127.0.0.1", 80)
-        val node2 = Instance("127.0.0.2", 80)
-        val node3 = Instance("127.0.0.3", 80)
-        val node4 = Instance("127.0.0.4", 80)
+        val node1 = Instance("http://127.0.0.1:8080")
+        val node2 = Instance("http://127.0.0.2:8080")
+        val node3 = Instance("http://127.0.0.3:8080")
+        val node4 = Instance("http://127.0.0.4:8080")
 
         // Act
         val actual = ConsistentHashRouter(
@@ -32,10 +32,10 @@ class ConsistentHashRouterTest {
     fun `add node of consistent hash`() {
         // Arrange
         val virtualNodeCount = 10
-        val node1 = Instance("127.0.0.1", 80)
-        val node2 = Instance("127.0.0.2", 80)
-        val node3 = Instance("127.0.0.3", 80)
-        val node4 = Instance("127.0.0.4", 80)
+        val node1 = Instance("http://127.0.0.1:8080")
+        val node2 = Instance("http://127.0.0.2:8080")
+        val node3 = Instance("http://127.0.0.3:8080")
+        val node4 = Instance("http://127.0.0.4:8080")
 
         val sut = ConsistentHashRouter(
             physicalNodes = listOf(node1, node2, node3),
@@ -54,10 +54,10 @@ class ConsistentHashRouterTest {
     fun `remove node of consistent hash`() {
         // Arrange
         val virtualNodeCount = 10
-        val node1 = Instance("127.0.0.1", 80)
-        val node2 = Instance("127.0.0.2", 80)
-        val node3 = Instance("127.0.0.3", 80)
-        val node4 = Instance("127.0.0.4", 80)
+        val node1 = Instance("http://127.0.0.1:8080")
+        val node2 = Instance("http://127.0.0.2:8080")
+        val node3 = Instance("http://127.0.0.3:8080")
+        val node4 = Instance("http://127.0.0.4:8080")
 
         val sut = ConsistentHashRouter(
             physicalNodes = listOf(node1, node2, node3, node4),
@@ -76,10 +76,10 @@ class ConsistentHashRouterTest {
     fun `get route node of consistent hash`() {
         // Arrange
         val virtualNodeCount = 10
-        val node1 = Instance("127.0.0.1", 80)
-        val node2 = Instance("127.0.0.2", 80)
-        val node3 = Instance("127.0.0.3", 80)
-        val node4 = Instance("127.0.0.4", 80)
+        val node1 = Instance("http://127.0.0.1:8080")
+        val node2 = Instance("http://127.0.0.2:8080")
+        val node3 = Instance("http://127.0.0.3:8080")
+        val node4 = Instance("http://127.0.0.4:8080")
 
         val requestIP = "192.168.0.4"
 
@@ -95,6 +95,6 @@ class ConsistentHashRouterTest {
         // hashValue: 38991125
         // 가장 가까운 노드의 hash: 103618170
         // 가장 근접하게 작은 노드의 hash: 34084152
-        assertThat(actual.getKey()).isEqualTo("127.0.0.3:80")
+        assertThat(actual.getKey()).isEqualTo("http://127.0.0.1:8080")
     }
 }
