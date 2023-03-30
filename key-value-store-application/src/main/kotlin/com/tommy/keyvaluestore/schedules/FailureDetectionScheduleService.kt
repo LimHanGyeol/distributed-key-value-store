e9505ee8345cc248c68882d440b73a3b1ea33508
limhangyeol <hang19663@gmail.com>
2023-03-30 15:48:05 +0900

    각 노드의 HeartBeat 를 수행하는 기능 구현
    * 스케줄링 시 본인 인스턴스에서 정수 값을 Sequential 하게 증가시키는 기능을 구현한다.
    * 이를 HeartBeat 를 대체할 값으로 이용한다.
    
    work items: #17

--- key-value-store-application/src/main/kotlin/com/tommy/keyvaluestore/schedules/FailureDetectionScheduleService.kt
+++ key-value-store-application/src/main/kotlin/com/tommy/keyvaluestore/schedules/FailureDetectionScheduleService.kt
@@ -1,15 +1,37 @@
 package com.tommy.keyvaluestore.schedules
 
+import java.net.InetAddress
 import mu.KotlinLogging
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.data.redis.core.ValueOperations
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
 @Service
-class FailureDetectionScheduleService {
+class FailureDetectionScheduleService(
+    @Value("\${server.port}")
+    val port: Int,
+    redisTemplate: StringRedisTemplate,
+) {
     private val logger = KotlinLogging.logger { }
+    private val valueOperations: ValueOperations<String, String> = redisTemplate.opsForValue()
 
     @Scheduled(initialDelay = 10000, fixedDelay = 10000)
     fun execute() {
         logger.info { "run failure detection schedule" }
+        val host = InetAddress.getLocalHost().hostAddress
+        countHeartBeat(host)
+    }
+
+    private fun countHeartBeat(host: String) {
+        val redisKey = "$host:$port"
+        val value = valueOperations.get(redisKey)
+        if (value == null) {
+            valueOperations.set(redisKey, "0")
+        }
+        val heartBeatCount = valueOperations.increment(redisKey)
+        valueOperations.set(redisKey, heartBeatCount.toString())
+        logger.info { "redisKey: $redisKey, heartBeat Count is $heartBeatCount" }
     }
 }
