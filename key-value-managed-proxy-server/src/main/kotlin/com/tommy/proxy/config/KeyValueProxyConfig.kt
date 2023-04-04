882ccb883f69a19d43b1a1172df798dba4b95221
limhangyeol <hang19663@gmail.com>
2023-04-04 23:31:30 +0900

    HTTP API 통신 기술 RestTemplate로 변경
    work items: #21

--- key-value-managed-proxy-server/src/main/kotlin/com/tommy/proxy/config/KeyValueProxyConfig.kt
+++ key-value-managed-proxy-server/src/main/kotlin/com/tommy/proxy/config/KeyValueProxyConfig.kt
@@ -1,37 +1,20 @@
 package com.tommy.proxy.config
 
-import io.netty.channel.ChannelOption
-import io.netty.handler.timeout.ReadTimeoutHandler
-import io.netty.handler.timeout.WriteTimeoutHandler
-import java.time.Duration
-import java.util.concurrent.TimeUnit
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpHeaders
-import org.springframework.http.MediaType
-import org.springframework.http.client.reactive.ReactorClientHttpConnector
-import org.springframework.web.reactive.function.client.WebClient
-import reactor.netty.http.client.HttpClient
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
+import org.springframework.web.client.RestTemplate
 
 @Configuration
 @EnableConfigurationProperties(KeyValueRoutesProperties::class)
 class KeyValueProxyConfig {
 
     @Bean
-    fun webClient(): WebClient {
-        val httpClient = HttpClient.create()
-            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 30000)
-            .responseTimeout(Duration.ofMillis(30000L))
-            .doOnConnected { connection ->
-                connection
-                    .addHandlerLast(ReadTimeoutHandler(30, TimeUnit.MILLISECONDS))
-                    .addHandlerLast(WriteTimeoutHandler(30, TimeUnit.MILLISECONDS))
-            }
+    fun restTemplate(): RestTemplate {
+        val factory = HttpComponentsClientHttpRequestFactory()
+        factory.setConnectTimeout(5000)
 
-        return WebClient.builder()
-            .clientConnector(ReactorClientHttpConnector(httpClient))
-            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-            .build()
+        return RestTemplate(factory)
     }
 }
--- key-value-managed-proxy-server/src/main/kotlin/com/tommy/proxy/services/KeyValueProxyService.kt
+++ key-value-managed-proxy-server/src/main/kotlin/com/tommy/proxy/services/KeyValueProxyService.kt
@@ -8,57 +8,42 @@ import com.tommy.proxy.dtos.KeyValueGetResponse
 import com.tommy.proxy.dtos.KeyValueSaveRequest
 import com.tommy.proxy.dtos.KeyValueSaveResponse
 import mu.KotlinLogging
-import org.springframework.http.HttpStatusCode
-import org.springframework.http.ResponseEntity
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
-import org.springframework.web.reactive.function.client.ClientResponse
-import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.bodyToMono
-import org.springframework.web.util.UriBuilder
-import reactor.core.publisher.Mono
-import reactor.core.scheduler.Schedulers
+import org.springframework.web.client.RestTemplate
 
 @Service
 class KeyValueProxyService(
-    private val webClient: WebClient,
+    private val restTemplate: RestTemplate,
     private val keyValueRoutesProperties: KeyValueRoutesProperties,
 ) {
     private val logger = KotlinLogging.logger { }
 
     fun put(keyValueSaveRequest: KeyValueSaveRequest): KeyValueSaveResponse {
-        val masterNodeIp = keyValueRoutesProperties.nodes.first()
+        val nodes = keyValueRoutesProperties.nodes.map { Instance(it) }
+        val hashFunction = MurmurHash3()
+        val consistentHashRouter = ConsistentHashRouter(nodes, VIRTUAL_NODE_COUNT, hashFunction)
+        val instance = consistentHashRouter.routeNode(keyValueSaveRequest.key) ?: throw RuntimeException()
 
-        val otherNodes = keyValueRoutesProperties.nodes
-            .filterNot { it == masterNodeIp }
-            .toList()
+        val nodeIp = instance.getKey()
 
-        // TODO: Master 에서 장애 발생 시 이를 감지하여 다른 노드로 전송하도록 해야한다.
-        val masterNodeResponse = callKeyValueSave(masterNodeIp, keyValueSaveRequest).block()
+        return try {
+            val headers = HttpHeaders()
+            headers.contentType = MediaType.APPLICATION_JSON
 
-        if (masterNodeResponse?.statusCode?.is2xxSuccessful == true) {
-            otherNodes.forEach {
-                val mono = callKeyValueSave(it, keyValueSaveRequest)
-                mono.subscribeOn(Schedulers.boundedElastic()).subscribe { println("Call Node : $it") }
-            }
-        }
-        return masterNodeResponse?.body!!
-    }
-
-    fun callKeyValueSave(
-        nodeIp: String,
-        keyValueSaveRequest: KeyValueSaveRequest,
-    ): Mono<ResponseEntity<KeyValueSaveResponse>> {
-        return webClient.mutate()
-            .baseUrl(nodeIp)
-            .build()
-            .post()
-            .bodyValue(keyValueSaveRequest)
-            .retrieve()
-            .onStatus(
-                { httpStatusCode: HttpStatusCode? -> httpStatusCode?.is5xxServerError == true },
-                { response: ClientResponse -> Mono.error(Throwable("Internal Server Error - try again later")) },
+            val responseEntity = restTemplate.postForObject(
+                nodeIp,
+                HttpEntity(keyValueSaveRequest, headers),
+                KeyValueSaveResponse::class.java,
             )
-            .toEntity(KeyValueSaveResponse::class.java)
+
+            responseEntity!!
+        } catch (e: Exception) {
+            logger.error { e.message }
+            throw RuntimeException(e.message)
+        }
     }
 
     fun get(key: String): KeyValueGetResponse {
@@ -70,20 +55,7 @@ class KeyValueProxyService(
         val nodeIp = instance.getKey()
 
         return try {
-            val keyValueGetResponse = webClient.mutate()
-                .baseUrl(nodeIp)
-                .build()
-                .get()
-                .uri { uriBuilder: UriBuilder ->
-                    uriBuilder
-                        .queryParam("key", key)
-                        .build()
-                }
-                .retrieve()
-                .bodyToMono<KeyValueGetResponse>()
-                .block()
-
-            keyValueGetResponse!!
+            restTemplate.getForObject("$nodeIp?key=$key", KeyValueGetResponse::class.java)!!
         } catch (e: Exception) {
             logger.error { e.message }
             throw RuntimeException(e.message)
