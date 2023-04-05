0c491795bd59ffca97aea3364b9d0abc59dbe237
limhangyeol <hang19663@gmail.com>
2023-04-05 17:00:34 +0900

    KeyValueProxyRunner 구현
    * Application 이 실행 시 Consistent Hashing 을 초기화하도록 한다.
    * initNodes 에서 seed 값을 null 로 전달할 경우에 random seed 를 생성하도록 한다.
    
    work items: #23

---
+++ key-value-managed-proxy-server/src/main/kotlin/com/tommy/proxy/services/KeyValueProxyRunner.kt
@@ -0,0 +1,20 @@
+package com.tommy.proxy.services
+
+import com.tommy.proxy.consistenthashing.ConsistentHashRouter
+import mu.KotlinLogging
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.stereotype.Component
+
+@Component
+class KeyValueProxyRunner(
+    val consistentHashRouter: ConsistentHashRouter,
+) : ApplicationRunner {
+
+    private val logger = KotlinLogging.logger { }
+
+    override fun run(args: ApplicationArguments?) {
+        consistentHashRouter.initNodes()
+        logger.info { "consistentHashRouter init nodes, hashring Size: ${consistentHashRouter.getHashRingSize()}" }
+    }
+}
