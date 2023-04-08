de8536b5e6f9faef14ea3652f281be8a28de71d6
limhangyeol <hang19663@gmail.com>
2023-04-08 23:06:00 +0900

    MurmurHash 의 seed 값을 port 로 설정
    * IpUtil 제거
    * IP 로 seed 값을 설정할 경우 인스턴스에 장애가 생기고 재실행 됐을 때 seed 로 인해 해시값이 변경되고, Hashing 의 정합성이 깨질 수 있다.
    * 0 은 아니되 고정값인 port 번호로 seed 값을 설정하여 인스턴스의 리더가 변경되더라도 Hash 값에 대한 정합성이 깨지지 않도록 한다.
    
    work items: #23

--- key-value-managed-proxy-server/src/main/kotlin/com/tommy/proxy/services/KeyValueProxyRunner.kt
+++ key-value-managed-proxy-server/src/main/kotlin/com/tommy/proxy/services/KeyValueProxyRunner.kt
@@ -1,23 +1,22 @@
 package com.tommy.proxy.services
 
 import com.tommy.proxy.consistenthashing.ConsistentHashRouter
-import com.tommy.proxy.utils.IpUtil
 import mu.KotlinLogging
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
 
 @Component
 class KeyValueProxyRunner(
+    @Value("\${server.port}") val port: Int,
     val consistentHashRouter: ConsistentHashRouter,
 ) : ApplicationRunner {
 
     private val logger = KotlinLogging.logger { }
 
     override fun run(args: ApplicationArguments?) {
-        val hostIp = IpUtil.getCurrentIp()
-        val seed = IpUtil.getIpToInteger(hostIp)
-        consistentHashRouter.initNodes(seed)
+        consistentHashRouter.initNodes(port)
         logger.info { "consistentHashRouter init nodes, hashring Size: ${consistentHashRouter.getHashRingSize()}" }
     }
 }
