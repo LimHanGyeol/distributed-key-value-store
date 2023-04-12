6eb73a782dd55abe47d30cc571bc778be2746b72
limhangyeol <hang19663@gmail.com>
2023-04-12 23:20:51 +0900

    장애 발생 노드 정보를 Proxy Server로 전송하는 기능 구현
    work items: #33

--- key-value-store-application/src/main/kotlin/com/tommy/keyvaluestore/configs/KeyValueStoreConfig.kt
+++ key-value-store-application/src/main/kotlin/com/tommy/keyvaluestore/configs/KeyValueStoreConfig.kt
@@ -1,10 +1,16 @@
 package com.tommy.keyvaluestore.configs
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.http.converter.StringHttpMessageConverter
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.client.RestTemplate
 import java.net.InetAddress
+import java.time.Duration
 
 @Configuration
 @EnableScheduling
@@ -14,5 +20,12 @@ class KeyValueStoreConfig {
     fun hostAddress(@Value("\${server.port}") port: Int): String {
         val hostIp = InetAddress.getLocalHost().hostAddress
         return "$hostIp:$port"
+    }
+
+    @Bean
+    fun restTemplate(restTemplateBuilder: RestTemplateBuilder): RestTemplate? {
+        return restTemplateBuilder.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .setConnectTimeout(Duration.ofMillis(5000)).setReadTimeout(Duration.ofMillis(5000))
+            .additionalMessageConverters(StringHttpMessageConverter(Charsets.UTF_8)).build()
     }
 }
