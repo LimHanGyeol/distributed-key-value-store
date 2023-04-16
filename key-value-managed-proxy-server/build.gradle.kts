9da0524724107619dd75b546debf067f4348eae6
limhangyeol <hang19663@gmail.com>
2023-04-17 00:04:03 +0900

    장애내성 모니터링을 위한 Prometheus, Actuator 설정
    work items: #44

--- key-value-managed-proxy-server/build.gradle.kts
+++ key-value-managed-proxy-server/build.gradle.kts
@@ -12,9 +12,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.redisson:redisson-spring-boot-starter:3.20.1")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
     implementation("com.google.guava:guava:31.1-jre")
+
+    implementation("io.micrometer:micrometer-core")
+    implementation("io.micrometer:micrometer-registry-prometheus")
 
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
