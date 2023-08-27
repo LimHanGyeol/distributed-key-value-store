a0c4961c9fec1e9fc2602e3338d23523b13a79eb
limhangyeol <hang19663@gmail.com>
2023-08-27 18:06:17 +0900

    dto 개선
    * data class가 누락된 부분 변경
    
    work items: #61

--- key-value-managed-proxy-server/src/main/kotlin/com/tommy/proxy/dtos/FailureResolutionRequest.kt
+++ key-value-managed-proxy-server/src/main/kotlin/com/tommy/proxy/dtos/FailureResolutionRequest.kt
@@ -1,25 +1,6 @@
 package com.tommy.proxy.dtos
 
-class FailureResolutionRequest(
+data class FailureResolutionRequest(
     val address: String,
     val heartBeatCount: Int,
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as FailureResolutionRequest
-
-        if (address != other.address) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return address.hashCode()
-    }
-
-    override fun toString(): String {
-        return "FaultNodeRequest(address='$address', heatBeatCount=$heartBeatCount)"
-    }
-}
+)
